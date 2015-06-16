83c29a9a36b4f27cb1ede2bb149f4295211d1cfa
Hannah Westberg <Hannah@Hannahs-MacBook-Air.local>
2015-06-15 19:43:07 -0500

    Exercise with working arithmetic functions.

--- public/arithmetic.php
+++ public/arithmetic.php
@@ -6,20 +6,29 @@ function add($a, $b)
 
 function subtract($a, $b)
 {
+    return $a - $b;
     // Add code here
 }
 
 function multiply($a, $b)
 {
+    return $a * $b;
     // Add code here
 }
 
 function divide($a, $b)
 {
+	return $a / $b;
     // Add code here
 }
 
-// Add code to test your functions here
+// Add code to test your functions hereg
+$a = 5;
+$b = 3;
 
+echo add($a, $b) . PHP_EOL;
+echo subtract($a, $b) . PHP_EOL;
+echo multiply($a, $b) . PHP_EOL;
+echo divide($a, $b) . PHP_EOL;
 
  ?>
